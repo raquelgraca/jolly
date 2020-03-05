5302347d7c364f32b2f8c8a4a77ee9f8ad9307fd
ramon godas <ramonunivigo@gmail.com>
2020-03-05 18:14:38 -0300

    finished pundit bookings

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -4,4 +4,25 @@ class BookingPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    record.user == 'parent'
+  end
+
+  def index?
+    record.user == user || record.play_session.user == user
+  end
+
+  def show?
+    record.user == user || record.play_session.user == user
+  end
+
+  def update?
+    record.user == user && record.play_session.appointment.start_time - 3.hours > DateTime.now
+  end
+
+  def destroy?
+    record.user == user && record.play_session.created_at - 15.minutes > DateTime.now
+  end
+
 end

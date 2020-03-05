07848c136daec109144b873b69393ab44959643e
ramon godas <ramonunivigo@gmail.com>
2020-03-05 17:33:21 -0300

    finished pundit play session

--- app/policies/play_session_policy.rb
+++ app/policies/play_session_policy.rb
@@ -4,4 +4,25 @@ class PlaySessionPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    record.user == 'recreation worker'
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
 end

12d97413d94da17e045c8ec43007240a243e02fb
gemma gonputh <gemmage3@hotmail.co.uk>
2020-03-05 12:27:15 -0300

    Validations Added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+validates  :role, presence: true
+validates  :first_name, presence: true
+validates  :last_name, presence: true
+validates  :street, presence: true
+validates  :street_number, presence: true
+validates  :neighbourhood, presence: true
+validates  :city, presence: true
+validates  :state, presence: true
+validates  :zip_code, presence: true
+validates  :avatar, presence: true
 end

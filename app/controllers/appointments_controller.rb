025a737a75db1204249089bcf27180a820234a63
Diego Unternaehrer <diego.unternaehrer@hotmail.com>
2020-03-06 12:59:17 -0300

    created actions in the appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,16 +1,45 @@
 class AppointmentsController < ApplicationController
   def index
+    @appointments = policy_scope(Appointment)
   end
+
   def show
+    @appointment = Appointment.find(params[:id])
+    authorize @appointment
   end
+
   def new
+    @appointment = Appointment.new
+    authorize @appointment
   end
+
   def create
+    @appointment = Appointment.create(appointment_params)
+    authorize @appointment
   end
+
   def edit
+    @appointment = Appointment.find(params[:id])
+    authorize @appointment
   end
+
   def update
+   @appointment.update(appointments_params)
+   authorize @appointment
+    if @appointment.save
+      redirect_to appointments_path
+    else
+      render :edit
+    end
   end
+
   def destroy
+    @appointment.destroy
+    authorize @appointment
+    redirect_to appointments_path
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(:minimum_capacity, :maximum_capacity, :start_time, :end_time)
   end
 end

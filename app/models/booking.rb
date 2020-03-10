dd76fbdf3f099e3615d9817a039770cef9c6f1de
raquelgraca <raquelngraca@gmail.com>
2020-03-10 12:38:14 -0300

    Created Bookings Controller and Booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,10 @@ class Booking < ApplicationRecord
   belongs_to :play_session
   belongs_to :user
 
+  validates :name_of_kid, presence: true
+  validates :gender_of_kid , presence: true
+  validates :age_of_kid, presence: true
+  validates :comment, presence: true
+
 end
+
--- app/models/play_session.rb
+++ app/models/play_session.rb
@@ -1,6 +1,7 @@
 class PlaySession < ApplicationRecord
   belongs_to :appointment
   belongs_to :user
+  has_many :bookings, dependent: :destroy
 
   has_one_attached :photo
   validates :name, presence: true

fbff1201b4f935c3e7e66e4a0fac10698ba7ae42
raquelgraca <raquelngraca@gmail.com>
2020-03-10 11:54:37 -0300

    Fixed booking model crop and paste WRONG

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,11 +2,4 @@ class Booking < ApplicationRecord
   belongs_to :play_session
   belongs_to :user
 
-  state:string
-  teddy_sku:string
-  amount:monetize
-  checkout_session_id:string
-  user:references
-  teddy:references
-
 end

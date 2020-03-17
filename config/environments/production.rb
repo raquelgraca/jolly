686f53dd8e4386430e91766a629fddc29edfb660
raquelgraca <raquelngraca@gmail.com>
2020-03-17 13:32:55 -0300

    fixed stripe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'jolly-br.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'jolly-br.com'}
 
 
   # Ignore bad email addresses and do not raise email delivery errors.

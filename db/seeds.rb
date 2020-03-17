09a1de9f40dc2c4fe842328d62973981799be658
raquelgraca <raquelngraca@gmail.com>
2020-03-17 08:46:13 -0300

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,9 @@ User.destroy_all
 
 puts "Creating 5 users..."
   parent_user01 = User.new(
-    first_name: "Mariana",
-    last_name: "Barreiras",
-    email: "mariana_b@email.com",
+    first_name: "Raquel",
+    last_name: "Graça",
+    email: "raquel@email.com",
     password: "123456",
     password_confirmation: "123456",
     role: "parent",
@@ -18,7 +18,7 @@ puts "Creating 5 users..."
     zip_code: "21200021",
     bio: "I love to be a mother",
     )
-  file = URI.open('https://i2.wp.com/www.sardiniauniqueproperties.com/wp-content/uploads/2015/10/Square-Profile-Pic-1-1.jpg')
+  file = URI.open('https://res.cloudinary.com/dxvk3qixb/image/upload/v1583937288/1NtBUkgPbZQsuXyVj4GzfASm.jpg')
   parent_user01.avatar.attach(io: file, filename: "parent_photo.jpg", content_type: "image/jpg")
   parent_user01.save!
 
@@ -35,11 +35,11 @@ puts "Creating 5 users..."
     city: "Rio de Janeiro",
     state: "RJ",
     zip_code: "21200021",
-    bio: "Experience working with youth populations representing the range of diverse communities in Rio de Janeiro.
+    bio: "I have 10 years of experience working with youth populations representing the range of diverse communities in Rio de Janeiro.
 Knowledge of and experience working with diverse communities and equity-seeking groups (ie Women, People with Disabilities, GLBT, Race and Ethnic groups).
 I have a certification in Parks and Recreation Rio de Janeiro's Principles of Healthy Child Development.",
     )
-  file = URI.open('https://i2.wp.com/www.sardiniauniqueproperties.com/wp-content/uploads/2015/10/Square-Profile-Pic-1-1.jpg')
+  file = URI.open('https://weddinghacker.com.br/wp-content/uploads/2017/05/opulent-profile-square-02.jpg')
   worker_user02.avatar.attach(io: file, filename: "worker02_photo.jpg", content_type: "image/jpg")
   worker_user02.save!
 
@@ -58,17 +58,17 @@ I have a certification in Parks and Recreation Rio de Janeiro's Principles of He
     zip_code: "2150021",
     bio: "My place is very cool!",
     )
-  file = URI.open('https://janecanblogdotcom.files.wordpress.com/2014/09/ashley-square-profile.jpg')
+  file = URI.open('https://oficinadainteligencia.com.br/wp-content/uploads/2019/07/opulent-profile-square-06.jpg')
   owner_user03.avatar.attach(io: file, filename: "owner03_photo.jpg", content_type: "image/jpg")
   owner_user03.save!
 
   worker_user04 = User.new(
     first_name: "Emilie",
-    last_name: "Tuxi",
-    email: "emilie_t@email.com",
+    last_name: "Nicolau",
+    email: "emilie_n@email.com",
     password: "123456",
     password_confirmation: "123456",
-    role: "play space owner",
+    role: "recreation worker",
     street: "Rua Banbina",
     street_number: "23",
     neighbourhood: "Botafogo",
@@ -77,7 +77,7 @@ I have a certification in Parks and Recreation Rio de Janeiro's Principles of He
     zip_code: "2150021",
     bio: "My place is very cool!",
     )
-  file = URI.open('https://janecanblogdotcom.files.wordpress.com/2014/09/ashley-square-profile.jpg')
+  file = URI.open('https://oficinadainteligencia.com.br/wp-content/uploads/2019/07/opulent-profile-square-01.jpg')
   worker_user04.avatar.attach(io: file, filename: "worker_user04_photo.jpg", content_type: "image/jpg")
   worker_user04.save!
 
@@ -116,18 +116,18 @@ I have a certification in Parks and Recreation Rio de Janeiro's Principles of He
     bio: "Experience planning and instructing preschool and children's programs. Formal training/certification in related area an asset.Skills in a variety of program areas i.e. music, art, sports, games.
 Current Emergency First Aid certification as recognized by the SUS.",
     )
-  file = URI.open('https://images.unsplash.com/photo-1441622915984-05d13dfb3d8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')
+  file = URI.open('https://oficinadainteligencia.com.br/wp-content/uploads/2019/07/opulent-profile-square-04.jpg')
   worker_user06.avatar.attach(io: file, filename: "worker06_photo.jpg", content_type: "image/jpg")
   worker_user06.save!
 
 
-  puts "Finished!"
+  puts "Finished!==============================================="
 
 PlaySpace.destroy_all
 
 owner = User.find_by(role: "play space owner")
 
-puts "Creating 8 Playspaces..."
+puts "Creating 8 Playspaces...+++++++++++++++++++++++++++++"
   play_space01 = PlaySpace.new(
     name: "Big and Tiny",
     street: "Rua da diversão",
@@ -200,7 +200,7 @@ puts "Creating 8 Playspaces..."
   play_space04.save!
 
   play_space05 = PlaySpace.new(
-    name: "Power Play Kids",
+    name: "Sky of diamonds",
     street: "Rua da Alegria",
     street_number: "32",
     neighbourhood: "Botafogo",
@@ -220,7 +220,7 @@ puts "Creating 8 Playspaces..."
   play_space05.save!
 
   play_space06 = PlaySpace.new(
-    name: "Power Play Kids",
+    name: "Floresta Encantada",
     street: "Rua da Alegria",
     street_number: "32",
     neighbourhood: "Botafogo",
@@ -240,7 +240,7 @@ puts "Creating 8 Playspaces..."
   play_space06.save!
 
   play_space07 = PlaySpace.new(
-    name: "Power Play Kids",
+    name: "Trenzinho da Alegria",
     street: "Rua da Alegria",
     street_number: "32",
     neighbourhood: "Botafogo",
@@ -263,20 +263,35 @@ puts "Creating 8 Playspaces..."
 
   Appointment.destroy_all
 
-  play_space_1 = PlaySpace.find_by(name: "Big and Tiny")
-  play_space_2 = PlaySpace.find_by(name: "Kids Island")
+  play_space01 = PlaySpace.find_by(name: "Big and Tiny")
+  play_space02 = PlaySpace.find_by(name: "Kids Island")
+  play_space03 = PlaySpace.find_by(name: "Fun Time")
+  play_space04 = PlaySpace.find_by(name: "Power Play Kids")
+  play_space05 = PlaySpace.find_by(name: "Sky of diamonds")
+  play_space06 = PlaySpace.find_by(name: "Floresta Encantada")
+  play_space07 = PlaySpace.find_by(name: "Trenzinho da Alegria")
   time_1 = DateTime.now + 2.hours
-  time_2 = 1.day.from_now
-  time_3 = DateTime.now + 1.houts
-  time_4 = DateTime.now + 0.5.hours
+  time_2 = DateTime.now + 4.hours
+  time_3 = DateTime.now + 1.hours
+  time_4 = DateTime.now + 3.hours
+  time_5 = DateTime.now + 5.hours
+  time_6 = DateTime.now + 7.hours
+  time_7 = DateTime.now + 1.hours
+  time_8 = DateTime.now + 3.hours
+  time_9 = DateTime.now + 4.hours
+  time_10 = DateTime.now + 6.hours
+  time_11 = DateTime.now + 5.hours
+  time_12 = DateTime.now + 7.hours
+  time_13 = DateTime.now + 8.hours
+  time_14 = DateTime.now + 10.hours
 
-  puts "Creating 4 Appointments...+++++++++++++++++++++++++++++++++++++"
+  puts "Creating 8 Appointments...+++++++++++++++++++++++++++++++++++++"
   appointment_1 = Appointment.create!(
     minimum_capacity: 5,
     maximum_capacity: 7,
     start_time: time_1,
     end_time: time_2,
-    play_space_id: play_space_1.id,
+    play_space_id: play_space01.id,
     owner_fee_per_kid_cents: 2000,
     )
   appointment_2 = Appointment.create!(
@@ -284,23 +299,55 @@ puts "Creating 8 Playspaces..."
     maximum_capacity: 7,
     start_time: time_3,
     end_time: time_4,
-    play_space_id: play_space_1.id,
+    play_space_id: play_space02.id,
     owner_fee_per_kid_cents: 1500,
     )
   appointment_3 = Appointment.create!(
     minimum_capacity: 10,
     maximum_capacity: 12,
-    start_time: time_1,
-    end_time: time_2,
-    play_space_id: play_space_2.id,
-    owner_fee_per_kid_cents: 1000,
+    start_time: time_5,
+    end_time: time_6,
+    play_space_id: play_space03.id,
+    owner_fee_per_kid_cents: 1300,
     )
   appointment_4 = Appointment.create!(
     minimum_capacity: 10,
     maximum_capacity: 12,
-    start_time: time_3,
-    end_time: time_4,
-    play_space_id: play_space_2.id,
+    start_time: time_7,
+    end_time: time_8,
+    play_space_id: play_space04.id,
+    owner_fee_per_kid_cents: 1600,
+    )
+  appointment_5 = Appointment.create!(
+    minimum_capacity: 5,
+    maximum_capacity: 7,
+    start_time: time_9,
+    end_time: time_10,
+    play_space_id: play_space05.id,
+    owner_fee_per_kid_cents: 1800,
+    )
+  appointment_6 = Appointment.create!(
+    minimum_capacity: 5,
+    maximum_capacity: 7,
+    start_time: time_11,
+    end_time: time_12,
+    play_space_id: play_space06.id,
+    owner_fee_per_kid_cents: 1500,
+    )
+  appointment_7 = Appointment.create!(
+    minimum_capacity: 10,
+    maximum_capacity: 12,
+    start_time: time_13,
+    end_time: time_14,
+    play_space_id: play_space07.id,
+    owner_fee_per_kid_cents: 1000,
+    )
+  appointment_8 = Appointment.create!(
+    minimum_capacity: 10,
+    maximum_capacity: 12,
+    start_time: time_11,
+    end_time: time_12,
+    play_space_id: play_space01.id,
     owner_fee_per_kid_cents: 1600,
     )
 
@@ -310,13 +357,13 @@ puts "Creating 8 Playspaces..."
 
   PlaySession.destroy_all
 
-  puts "Creating 4 Play_sessions..."
+  puts "Creating 08 Play_sessions...++++++++++++++++++++++++++++"
 
     play_session01 = PlaySession.new(
     name: "Afternoon Reading",
     description: "Afternoon reading for under 10s",
     requirements: "Under 10",
-    worker_fee_per_kid_cents: 1000,
+    worker_fee_per_kid_cents: 1200,
     user_id: worker_user04.id,
     status: "confirmed",
     appointment_id: appointment_1.id,
@@ -331,8 +378,8 @@ puts "Creating 8 Playspaces..."
     name: "Tuesday Swimming",
     description: "Two hours supervised swimming",
     requirements: "Armbands needed",
-    worker_fee_per_kid_cents: 4000,
-    user_id: worker_user03.id,
+    worker_fee_per_kid_cents: 1400,
+    user_id: worker_user02.id,
     status: "cancelled",
     appointment_id: appointment_2.id,
     )
@@ -346,7 +393,7 @@ puts "Creating 8 Playspaces..."
     description: "Colouring, clay play, drawings",
     requirements: "Aged 4 - 11",
     worker_fee_per_kid_cents: 1500,
-    user_id: worker_user03.id,
+    user_id: worker_user05.id,
     status: "confirmed",
     appointment_id: appointment_3.id,
     )
@@ -361,7 +408,7 @@ puts "Creating 8 Playspaces..."
     name: "Toy and object play",
     description: "Group session with a variety of toys",
     requirements: "Aged 4-11",
-    worker_fee_per_kid_cents: 1000,
+    worker_fee_per_kid_cents: 900,
     user_id: worker_user04.id,
     status: "pending",
     appointment_id: appointment_4.id,
@@ -371,13 +418,85 @@ puts "Creating 8 Playspaces..."
   play_session04.photo.attach(io: file13, filename: "play_session04", content_type: "image/jpg")
   play_session04.save!
 
-  puts "Finished!"
+    play_session06 = PlaySession.new(
+    name: "Jump class",
+    description: "Improve your balance and have fun in a super elastic playground.",
+    requirements: "Aged 8-11",
+    worker_fee_per_kid_cents: 1100,
+    user_id: worker_user06.id,
+    status: "pending",
+    appointment_id: appointment_5.id,
+    )
+
+  file13 = URI.open('https://res.cloudinary.com/dxvk3qixb/image/upload/v1583936835/oUGCDS3XsudgX31m3L4fPkmr.jpg')
+  play_session06.photo.attach(io: file13, filename: "play_session04", content_type: "image/jpg")
+  play_session06.save!
+
+    play_session07 = PlaySession.new(
+    name: "Painting Session",
+    description: "Children of all ages love to paint! From the simplest painting with a brush or hands to more elaborate spin art, marble painting, and painting a fort.",
+    requirements: "Aged 08-12",
+    worker_fee_per_kid_cents: 1600,
+    user_id: worker_user02.id,
+    status: "pending",
+    appointment_id: appointment_6.id,
+    )
+
+  file13 = URI.open('https://res.cloudinary.com/dxvk3qixb/image/upload/v1584134616/j6ftsMwDnewLLAqYxVPNpLMH.jpg')
+  play_session07.photo.attach(io: file13, filename: "play_session04", content_type: "image/jpg")
+  play_session07.save!
+
+  play_session08 = PlaySession.new(
+    name: "Drama class",
+    description: "Drama and imaginative play classes for children aged 5 years to 11 Years.",
+    requirements: "Aged 5-11",
+    worker_fee_per_kid_cents: 1200,
+    user_id: worker_user05.id,
+    status: "pending",
+    appointment_id: appointment_7.id,
+    )
+
+  file13 = URI.open('https://images.unsplash.com/photo-1485546246426-74dc88dec4d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')
+  play_session08.photo.attach(io: file13, filename: "play_session04", content_type: "image/jpg")
+  play_session08.save!
+
+
+  play_session09 = PlaySession.new(
+    name: "Managing Emotions",
+    description: "Mental health literacy and emotional self-regulation",
+    requirements: "Aged 12-15",
+    worker_fee_per_kid_cents: 1250,
+    user_id: worker_user06.id,
+    status: "pending",
+    appointment_id: appointment_8.id,
+    )
+
+  file13 = URI.open('https://images.unsplash.com/photo-1524503033411-c9566986fc8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80')
+  play_session09.photo.attach(io: file13, filename: "play_session04", content_type: "image/jpg")
+  play_session09.save!
+
+  play_session10 = PlaySession.new(
+    name: "Playground session",
+    description: "Group session at one of the largest playgrounds in Rio.",
+    requirements: "Aged 9-11",
+    worker_fee_per_kid_cents: 1000,
+    user_id: worker_user05.id,
+    status: "pending",
+    appointment_id: appointment_1.id,
+    )
+
+  file13 = URI.open('https://res.cloudinary.com/dxvk3qixb/image/upload/v1584019750/cRncYD26um6mTqhbvaKWoWBi.jpg')
+  play_session10.photo.attach(io: file13, filename: "play_session04", content_type: "image/jpg")
+  play_session10.save!
+
+
+  puts "Finished!======================================="
 
   Booking.destroy_all
 
   parent = User.find_by(role: "parent")
 
-  puts "Creating 4 Bookings..."
+  puts "Creating 4 Bookings...++++++++++++++++++++++++++"
 
     booking01 = Booking.create!(
     name_of_kid: "Joseph",
@@ -395,7 +514,7 @@ puts "Creating 8 Playspaces..."
     age_of_kid: 5,
     comment: "He likes everything except play videogames",
     status: "cancelled",
-    play_session_id: play_session02.id,
+    play_session_id: play_session03.id,
     user_id: parent.id,
     )
 
@@ -403,9 +522,9 @@ puts "Creating 8 Playspaces..."
     name_of_kid: "Elisa",
     gender_of_kid: "Female",
     age_of_kid: 9,
-    comment: "She tried to kill me once",
+    comment: "She can't eat peanut.",
     status: "confirmed",
-    play_session_id: play_session03.id,
+    play_session_id: play_session06.id,
     user_id: parent.id,
     )
 
@@ -415,42 +534,42 @@ puts "Creating 8 Playspaces..."
     age_of_kid: 8,
     comment: "She loves to take a nap after lunch",
     status: "pending",
-    play_session_id: play_session04.id,
+    play_session_id: play_session08.id,
     user_id: parent.id,
     )
 
-  puts "Finished!============================================="
+  puts "Finished!=============================="
 
   Review.destroy_all
 
-  puts "Creating 4 reviews++++++++++++++++++++++++++++"
+  puts "Creating 4 reviews+++++++++++++++++++++"
 
     review01 = Review.create!(
       content: "My child had an amazing time! The class was very well organized and prepared.",
       stars: 5,
       reviewer_id: parent_user01.id,
-      reviewee_id: worker_user03.id,
+      reviewee_id: worker_user02.id,
       )
 
     review02 = Review.create!(
-      content: "Nice ammenities but the recreational worker could be better",
+      content: "Nice ammenities and the recreational worker is very professional and carefull with all the kids.",
       stars: 4,
-      reviewer_id: parent_user02.id,
-      reviewee_id: worker_user03.id,
+      reviewer_id: parent_user01.id,
+      reviewee_id: worker_user04.id,
       )
 
     review03 = Review.create!(
-      content: "Such an old place",
-      stars: 2,
-      reviewer_id: parent_user02.id,
-      reviewee_id: worker_user03.id,
+      content: "He caught everyone's attention by telling incredible stories",
+      stars: 4,
+      reviewer_id: parent_user01.id,
+      reviewee_id: worker_user05.id,
       )
 
     review04 = Review.create!(
-      content: "Lovely place",
+      content: "Lovely work of this recreation. 5 deserved stars!",
       stars: 5,
       reviewer_id: parent_user01.id,
-      reviewee_id: worker_user03.id,
+      reviewee_id: worker_user06.id,
       )
 
   puts "Finished!================================"

8ca69a7393abab680e74df0f43b63d79bde95de6
raquelgraca <raquelngraca@gmail.com>
2021-06-22 20:49:42 -0300

    added text in PT for the jollyDay page

--- app/controllers/jolly_days_controller.rb
+++ app/controllers/jolly_days_controller.rb
@@ -1,3 +1,24 @@
 class JollyDaysController < ApplicationController
+  def new
+    @jolly_day = JollyDay.new
+  end
+
+  def create
+    @jolly_day = JollyDay.new(jolly_day_params)
+    if @jolly_day.save
+      redirect_to jolly_day_path(@jolly_day)
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  private
+  def jolly_day_params
+    params.require(:jolly_day).permit( :name, :start, :end, :maximum_capacity, :minimum_age, :maximum_age)
+  end
 
 end

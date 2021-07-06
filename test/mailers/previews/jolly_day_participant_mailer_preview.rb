6567aa03695cd9a722d9e49ee44e58272f31ae28
Diego Unternaehrer <diego.unternaehrer@hotmail.com>
2021-07-06 22:35:30 +0200

    Set up test with first jolly day participant

--- test/mailers/previews/jolly_day_participant_mailer_preview.rb
+++ test/mailers/previews/jolly_day_participant_mailer_preview.rb
@@ -3,7 +3,8 @@ class JollyDayParticipantMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/jolly_day_participant_mailer/confirmation
   def confirmation
-    JollyDayParticipantMailer.confirmation
+    jolly_day_participant = JollyDayParticipant.first
+    JollyDayParticipantMailer.with(jolly_day_participant: jolly_day_participant).confirmation
   end
 
 end
